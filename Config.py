f92592e5c16deac0730bbccbb0bcdf9b797c67e3
dquint54 <dquint54@gmail.com>
2022-07-11 17:44:49 -0700

    Got captcha by pass to work temporally

--- Config.py
+++ Config.py
@@ -1,1 +1,5 @@
 CHROME_DRIVER = "/Users/Q/PycharmProjects/chromedriver.exe"
+
+Username: 'dquint54@gmail.com'
+Password: 'Quintanilla54'
+
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from Config import *
 from Bypass import *
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -15,18 +13,20 @@ from selenium.webdriver.support import expected_conditions as EC
 
 if __name__ == '__main__':
 
-    # driver = webdriver.Chrome(executable_path=CHROME_DRIVER)
-    # driver.get("https://www.amazon.com")
-    # time.sleep(5)
-
-    Check()
-
-
-
-    button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "nav-action-inner")))
+    outcome = Check()
+    if outcome == True:
+        Bypass()
+    else:
+        print('no bypass needed')
 
 
-    driver.quit()
+
+
+        button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "Sign in securely")))
+        button.click()
+
+
+
 
 
 
